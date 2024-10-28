da8ae702f26f3512ad20cf2c6abdc4c7d849de4b
Fernando Tavares <110138879+fertavares@users.noreply.github.com>
2024-10-28 15:20:47 -0300

    mudanças em PLN_SC.py
    remoção do botão de seleção, tendo o banco songs.csv por padrão

--- src/PLN_SC.py
+++ src/PLN_SC.py
@@ -20,9 +20,9 @@ class IDFApp:
         self.root.resizable(width=False, height=False)
         self.root.title("Recomendações de Música (Similiaridade dos Cossenos)")
         self.root.geometry("800x900")
-        #customtkinter.deactivate_automatic_dpi_awareness()
+        customtkinter.deactivate_automatic_dpi_awareness()
         self.root.configure(bg="#f0f0f0")
-        self.root.iconbitmap(r"../img/favicon.ico") # Converte .png etc para .ico / salvar como favicon.ico na pasta img 
+        self.root.iconbitmap(r"../GitRepositorio-main/img/favicon.ico") # Converte .png etc para .ico / salvar como favicon.ico na pasta img 
         
         estilo_titulo = ("Arial", 14, "bold")
         estilo_normal = ("Arial", 14)
@@ -32,53 +32,56 @@ class IDFApp:
         for i in range(2):
             self.root.grid_columnconfigure(i, weight=1)
        
-        self.label = customtkinter.CTkLabel(root, text="Escoha o Arquivo CSV:", font=estilo_titulo)
-        self.label.grid(row=0, column=0, columnspan=2, pady=10, padx=20, sticky='w')
+        #self.label = customtkinter.CTkLabel(root, text="Escoha o Arquivo CSV:", font=estilo_titulo)
+        #self.label.grid(row=0, column=0, columnspan=2, pady=10, padx=20, sticky='w')
         
-        self.btn_upload = customtkinter.CTkButton(root, text="Escolher", font=estilo_normal, command=self.upload_file_text)
-        self.btn_upload.grid(row=1, column=0, columnspan=2, pady=10, padx=150, sticky="nsew") 
+        #self.btn_upload = customtkinter.CTkButton(root, text="Escolher", font=estilo_normal, command=self.upload_file_text, state=tk)
+        #self.btn_upload.grid(row=1, column=0, columnspan=2, pady=10, padx=150, sticky="nsew") 
         
         self.lbl_colunas = customtkinter.CTkLabel(root, text="Selecione a Coluna:", font=estilo_titulo)
-        self.lbl_colunas.grid(row=2, column=0, pady=10, padx=20, sticky='w')
+        self.lbl_colunas.grid(row=0, column=0, pady=10, padx=20, sticky='w')
         
         self.lbx_colunas = tk.Listbox(root, selectmode=tk.SINGLE, width=40, height=8, font=estilo_normal, bd=2, relief='solid')
-        self.lbx_colunas.grid(row=3, column=0, columnspan=2, pady=10, padx=50, sticky="nsew")
+        self.lbx_colunas.grid(row=1, column=0, columnspan=2, pady=10, padx=50, sticky="nsew")
         
         self.lbl_amostra = customtkinter.CTkLabel(root, text="Insira a Amostra:", font=estilo_titulo)
-        self.lbl_amostra.grid(row=4, column=0, columnspan=2, pady=10, padx=20, sticky='w')
+        self.lbl_amostra.grid(row=2, column=0, columnspan=2, pady=10, padx=20, sticky='w')
         
         self.txt_amostra = tk.Text(root, height=5, width=40, font=estilo_normal, bd=2, relief="solid")
-        self.txt_amostra.grid(row=5, column=0, columnspan=2, pady=10, padx=20, sticky="nsew")
+        self.txt_amostra.grid(row=3, column=0, columnspan=2, pady=10, padx=20, sticky="nsew")
 
         self.btn_calcIDF = customtkinter.CTkButton(root, text="Calcular", font=estilo_normal, command=self.calcular_similiaridade_cosseno_text)
-        self.btn_calcIDF.grid(row=6, column=0, columnspan=2, pady=20, padx=150, sticky="nsew")
+        self.btn_calcIDF.grid(row=4, column=0, columnspan=2, pady=20, padx=150, sticky="nsew")
         
         self.lbl_resultado = customtkinter.CTkLabel(root, text="Resultado:", font=estilo_titulo)
-        self.lbl_resultado.grid(row=7, column=0, columnspan=2, padx=20, sticky='w')
-        
+        self.lbl_resultado.grid(row=5, column=0, columnspan=2, padx=20, sticky='w')
+
         self.txt_resultado = tk.Text(root, height=8, width=60, font=estilo_normal, bd=2, relief='solid', state="disabled")
-        self.txt_resultado.grid(row=8, column=0, columnspan=2, pady=10, padx=20, sticky="nsew")
+        self.txt_resultado.grid(row=6, column=0, columnspan=2, pady=10, padx=20, sticky="nsew")
         
         self.txt_elementos_analisados = customtkinter.CTkLabel(root, text=f"De xxx \nelementos analisados", font=estilo_titulo, text_color="lightgreen")
-        self.txt_elementos_analisados.grid(row=8, column=2, columnspan=2, pady=10, padx=20, sticky='nsew')
+        self.txt_elementos_analisados.grid(row=7, column=2, columnspan=2, pady=10, padx=20, sticky='nsew')
 
         self.todas_colunas = []  # Armazena todas as colunas do arquivo CSV
-                
-    def upload_file_text(self):
-        file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
-        if file_path:
+
+        self.load_csv_file()
+
+    def load_csv_file(self):
+        try:
+            # Tenta abrir o arquivo como UTF-8
+            self.data = pd.read_csv(r"../GitRepositorio-main/DataFrame/Songs.csv", encoding='utf-8') 
+            self.gera_colunas_listbox()
+
+        except UnicodeDecodeError: 
+            # Corrige o erro se o arquivo não estiver em utf-8
             try:
-                self.data = pd.read_csv(file_path, encoding='utf-8') # Tenta abrir como UTF-8
+                # Tenta abrir com ISO-8859-1
+                self.data = pd.read_csv(r"../GitRepositorio-main/DataFrame/Songs.csv", encoding='ISO-8859-1')  
                 self.gera_colunas_listbox()
-                
-            except UnicodeDecodeError: # Corrige o erro se o arquivo não estiver em utf-8
-                try:
-                    self.data = pd.read_csv(file_path, encoding='ISO-8859-1')  # Tenta abrir com ISO-8859-1
-                    self.gera_colunas_listbox()
-                    
-                except Exception as e:
-                    messagebox.showerror("[Erro!]", f"[Erro!] : {e}") 
-    
+
+            except Exception as e:
+                messagebox.showerror("[Erro!]", f"[Erro!] : {e}") 
+
     def gera_colunas_listbox(self):
         self.todas_colunas = list(self.data.columns)  # columns Propriedade DataFrame Pandas
         self.lbx_colunas.delete(0, tk.END)
@@ -105,7 +108,7 @@ class IDFApp:
             vectorizer = TfidfVectorizer(use_idf=True, stop_words="english")
             
             stop_words = vectorizer.get_stop_words()
-            
+     
             amostra = ' '.join(palavra for palavra in amostra.split() if palavra.lower() not in stop_words)
         
             tfidf_matriz = vectorizer.fit_transform(self.data[colunas_selecionadas].fillna('').astype(str).apply(lambda x: ' '.join(palavra for palavra in ' '.join(x).split() if palavra.lower() not in stop_words), axis=1).tolist() + [amostra])
